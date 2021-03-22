e43d0aef9a3be253de5115bbc3ac7962506e0a0b
Inio Idoate <idoate.127253@e.unavarra.es>
2021-03-23 00:55:41 +0100

    Ejercicio08

---
+++ practicas/practica05/ejercicio08.js
@@ -0,0 +1,13 @@
+var counter = [0,0,0];
+var lastObject = ["Mostrar contenidos","Mostrar contenidos","Mostrar contenidos"];
+label1.appendChild(label1text);
+for (var i= 0 ; i<3 ; i++){
+    lastObject[i] =label1;
+}
+function myFunction(index){
+    var actualObject = document.getElementById(index).innerHTML;
+    var newObject = lastObject[index];
+    lastObject[index]=actualObject;
+    document.getElementById(index).innerHTML = newObject;
+
+}
